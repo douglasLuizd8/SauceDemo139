c046db8c998af95494acc257a3ff972278ffc83f
douglasLuizd8 <douglas.luiz.santos@outlook.com>
2023-12-03 17:57:10 -0300

    Início do Page Object

---
+++ Pages/InventoryPage.cs
@@ -0,0 +1,22 @@
+using OpenQA.Selenium;
+
+namespace Pages
+{
+    public class InventoryPage : CommonPage //  os dois pontos (:) aqui significa extends
+    {
+        // Mapeamento dos Elementos
+        // Como queremos mapear o botão "Add to Cart" e o id muda por produto
+        // Iremos mapear de forma dinâmica pelo bloco de Ações
+
+        // Construtor
+        public InventoryPage(IWebDriver driver) : base(driver){}
+
+        // Ações
+        public void AdicionarAoCarrinho(String produto)
+        {
+            String seletorProduto = "add-to-cart-" + produto.ToLower().Replace(" ", "-");
+            driver.FindElement(By.Id(seletorProduto)).Click();
+        }
+
+    }
+}
